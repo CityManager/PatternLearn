0bf56c8749a09b641bae280dabd6db5b6e26197d
Weiman Xu <weiman_gz@163.com>
2018-02-11 16:29:18 +0800

    BlockingQueue

---
+++ src/main/java/ind/xwm/basic/pattern/creatorCustomer/blockQueue/BlockingQueuePhoneStorage.java
@@ -0,0 +1,27 @@
+package ind.xwm.basic.pattern.creatorCustomer.blockQueue;
+
+import ind.xwm.basic.pattern.creatorCustomer.Storage;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueuePhoneStorage<T> implements Storage<T> {
+    private int series;
+
+    private BlockingQueue<T> phoneQueue = new ArrayBlockingQueue<>(10);
+
+    @Override
+    public synchronized int getSeries() {
+        return series++;
+    }
+
+    @Override
+    public void storage(T t) throws Exception {
+        phoneQueue.put(t);
+    }
+
+    @Override
+    public T takeOut() throws Exception {
+        return phoneQueue.take();
+    }
+}
